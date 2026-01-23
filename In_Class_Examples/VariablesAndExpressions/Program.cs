fc574832c089e1b31b0ce400660e0582f974d7ea
Ackerman, Adam P. <adam@ou.edu>
2026-01-23 10:50:56 -0600

    Completed in class example over variables and expressions

--- In_Class_Examples/VariablesAndExpressions/Program.cs
+++ In_Class_Examples/VariablesAndExpressions/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// datatype variablename = value;
+double favoriteNumber = 3;
+string name;
+name = "Adam";
+
+double anotherNumber = 7;
+double sum = favoriteNumber + anotherNumber;
+
+string message = "Hello, " + name + "! Your favorite number is " + favoriteNumber.ToString() + ".";
+message = $"Hello, {name}! Your favorite number is {favoriteNumber}.";
 Console.WriteLine("Hello, World!");
+Console.WriteLine(message);
