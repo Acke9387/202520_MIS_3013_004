8cc563c025dde12d3d6c6ad25bf1d25d9e7dbe87
Ackerman, Adam P. <adam@ou.edu>
2026-02-02 10:30:56 -0600

    Completed participation for toin coss

--- Participations/Conditional_Coin_Toss/Program.cs
+++ Participations/Conditional_Coin_Toss/Program.cs
@@ -1,4 +1,37 @@
-﻿
+﻿//const int HEADS = 0;
+//const int TAILS = 1;
+
+
+Console.WriteLine("Welcome to my Coin guessing game");
 
 Random r = new Random();
 int toss = r.Next(0, 2); // Generates a number between 0 and 1
+
+/*
+ * Heads = 0
+ * Tails = 1
+ */
+string tossAsString;
+if (toss == 0) 
+{
+    tossAsString = "Heads";
+}
+else// if (toss == 1)
+{  
+    tossAsString = "Tails";
+}
+//Console.WriteLine(tossAsString); // For testing purposes only, remove or comment out in production
+
+Console.WriteLine("Please guess either 'Heads' or 'Tails' <<");
+string userGuess = Console.ReadLine(); // Users guess of either Heads or Tails
+
+Console.WriteLine($"The coin came up {tossAsString}");
+
+if (userGuess.ToLower() == tossAsString.ToLower())
+{
+    Console.WriteLine("You guessed correctly!");
+}
+else
+{
+    Console.WriteLine("Sorry, better luck next time!");
+}
