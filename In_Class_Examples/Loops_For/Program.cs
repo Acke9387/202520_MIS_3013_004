328d2073a684f38c53ce7e494d328127ca2f3e3e
Ackerman, Adam P. <adam@ou.edu>
2026-02-02 10:47:42 -0600

    Completed in class participations over loops

---
+++ In_Class_Examples/Loops_For/Program.cs
@@ -0,0 +1,27 @@
+﻿//In class example 3
+Console.WriteLine("What value (maximum) would you like to perform FizzBuzz to? <<");
+//string input = Console.ReadLine(); 
+//int upperBound = int.Parse(input);
+
+int upperBound = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= upperBound; i = i + 1)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine($"Fizzbuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+}
---
+++ In_Class_Examples/Loops_While/Program.cs
@@ -0,0 +1,14 @@
+﻿// Validate users input as a number
+
+Console.WriteLine("Please enter your favorite number <<");
+//int favoriteNumber = int.Parse(Console.ReadLine());
+int favoriteNumber;
+string input = Console.ReadLine();
+
+while (int.TryParse(input, out favoriteNumber) == false)
+{
+    Console.WriteLine("That's an invalid input, please try again <<");
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(favoriteNumber);
