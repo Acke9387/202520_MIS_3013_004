7a51535448a8247232b3c001eef1c2a7ea9f4a01
Ackerman, Adam P. <adam@ou.edu>
2026-02-11 10:45:33 -0600

    Completed in class example over string manipulation

---
+++ In_Class_Examples/Stringmanipulation_Example1/Program.cs
@@ -0,0 +1,12 @@
+﻿string sentence = "Acres of almond trees lined the interstate highway which complimented the crazy driving nuts.";
+
+Console.WriteLine($"There are {sentence.Length} letters in the string:\n");
+Console.WriteLine($"The last letter of the sentence is {sentence[sentence.Length - 1]}");
+
+sentence = sentence.ToUpper();
+// Loop through the string and print every other letter
+for (int i = 0; i < sentence.Length; i = i + 2)
+{
+    Console.WriteLine(sentence[i]);
+}
+
