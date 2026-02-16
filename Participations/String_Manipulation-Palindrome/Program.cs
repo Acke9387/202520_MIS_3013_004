5d128beb0992f7faa7e5f1356c8aa5c34c7d5c9b
Ackerman, Adam P. <adam@ou.edu>
2026-02-16 10:06:42 -0600

    completed participation over palindrome

---
+++ Participations/String_Manipulation-Palindrome/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please enter a word to see if it is a palindrome. <<");
+string word = Console.ReadLine();
+
+string reversedWord = "";
+
+for (int i = word.Length - 1; i >= 0; i = i - 1)
+{
+    reversedWord = reversedWord + word[i];
+}
+
+if (word == reversedWord)
+{
+    Console.WriteLine("The word is a palindrome.");
+}
+else
+{
+    Console.WriteLine("The word is not a palindrome.");
+}
