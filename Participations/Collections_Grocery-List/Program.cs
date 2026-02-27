33ce3cef637dd5fa730ecb1270d5d218367db9d3
Ackerman, Adam P. <adam@ou.edu>
2026-02-27 10:08:47 -0600

    Completed participation over lists

---
+++ Participations/Collections_Grocery-List/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Welcome to the Grocery List creator.");
+Console.WriteLine("You may start entering in items for your list, simply type 'done' when you are finished <<");
+
+List<string> groceryItems = new List<string>();
+string input;
+do
+{
+
+    input = Console.ReadLine();
+    if (input.ToLower() != "done")// && groceryItems.Contains(input) == false)
+    {
+        if (groceryItems.Contains(input))
+        {
+            Console.WriteLine("That item is already on the list, please enter a different item.");
+        }
+        else
+        {
+            groceryItems.Add(input);
+        }
+    }
+
+} while (input.ToLower() != "done");
+
+Console.WriteLine("\nFinal Grocery List");
+Console.WriteLine("".PadLeft(20,'_'));
+foreach (string item in groceryItems)
+{
+    Console.WriteLine($"-\t{item}");
+}
