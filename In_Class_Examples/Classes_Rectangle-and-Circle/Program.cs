8d0f0b9a90013685960c20587c3d33695f125c54
Ackerman, Adam P. <adam@ou.edu>
2026-03-25 10:50:53 -0500

    Did in class example over classes

---
+++ In_Class_Examples/Classes_Rectangle-and-Circle/Program.cs
@@ -0,0 +1,15 @@
+﻿using Classes_Rectangle_and_Circle;
+
+Rectangle r = new Rectangle();
+
+r.Length = 5;
+r.Width = 10;
+
+double perimeter = r.CalculatePerimeter();
+Console.WriteLine(r);
+
+
+Rectangle r2 = new Rectangle();
+r2.Length = 29;
+r2.Width = 101;
+Console.WriteLine(r2.ToString());
---
+++ In_Class_Examples/Classes_Rectangle-and-Circle/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Rectangle_and_Circle
+{
+    public class Rectangle
+    {
+        /// <summary>
+        /// Gets or sets the length value.
+        /// </summary>
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+
+
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+
+        /// <summary>
+        /// Calculatex the perimeter of the rectangle
+        /// </summary>
+        /// <returns>A double representing the perimeter value</returns>
+        public double CalculatePerimeter()
+        {
+            double perimeter = 0;
+
+            perimeter = 2 * (Length + Width);
+
+            return perimeter;
+        }
+
+
+        public double CalculateArea()
+        {
+            double area = 0;
+
+            area = Width * Length;
+
+            return area;
+        }
+
+        public override string ToString()
+        {
+            double p = CalculatePerimeter();
+            double a = CalculateArea();
+            return $"The rectangle has a length of {Length}, a width of {Width}, a perimeter of {p} and an area of {a}";
+        }
+
+    }
+}
