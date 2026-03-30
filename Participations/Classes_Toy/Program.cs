5e54c39259803599b246c08203d4f17a0de857c9
Ackerman, Adam P. <adam@ou.edu>
2026-03-30 10:19:45 -0500

    Completed participation over Toys

---
+++ Participations/Classes_Toy/Program.cs
@@ -0,0 +1,30 @@
+﻿using Classes_Toy;
+
+List<Toy> toys = new List<Toy>();
+
+Toy t1 = new Toy();
+t1.Manufacturer = "Hasbro";
+t1.Name = "Monopoly";
+t1.Price = 19.99;
+//t1.Notes = "Classic board game";
+t1.AddNotes("Classic board game");
+t1.AddNotes("Great for family game night");
+
+Console.WriteLine(t1);
+
+Toy t2 = new Toy();
+t2.Manufacturer = "Lego";
+t2.Name = "Star Wars Millennium Falcon";
+t2.Price = 159.99;
+t2.AddNotes("Detailed model of the Millennium Falcon");
+
+Console.WriteLine(t2);
+toys.Add(t1);
+toys.Add(t2);
+
+
+//while (true)
+//{
+//    Toy toy = new Toy();
+//    toys.Add(toy);
+//}
---
+++ Participations/Classes_Toy/Toy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Notes;
+
+        public void AddNotes(string note)
+        {
+            Notes += note + Environment.NewLine;
+        }
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Notes = string.Empty;
+            Price = 0.0;
+        }
+
+        public string GetAisle()
+        {
+            string aisle = "";
+            Random r = new Random();
+            aisle = Manufacturer.ToUpper()[0].ToString();
+            aisle += r.Next(1, 25);
+
+            return aisle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} by {Manufacturer} costs ${Price:F2} and is found on {GetAisle()}. Notes:{Notes}";
+
+        }
+    }
+}
