9c62edfbfa67313b6a747c66898903c4f888c797
Ackerman, Adam P. <adam@ou.edu>
2026-04-01 10:24:52 -0500

    completed participation over the toybox

---
+++ Participations/Classes_ToyBox/ToyBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_ToyBox
+{
+    public class ToyBox
+    {
+        public List<Toy> Toys { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Location { get; set; }
+
+        public ToyBox()
+        {
+            Toys = new List<Toy>();
+            Owner = string.Empty;
+            Location = string.Empty;
+        }
+
+        public Toy GetRandomToy()
+        {
+            Random random = new Random();
+            int toyIndex = random.Next(0, Toys.Count);
+
+            return Toys[toyIndex];
+        }
+
+        public override string ToString()
+        {
+            return $"{Owner}'s toy box is located in the {Location} and contains {Toys.Count} toys.";
+
+            string message = $"{Owner}'s toy box is located in the {Location} and contains {Toys.Count} toys.";
+            foreach (Toy toy in Toys)
+            {
+                message += Environment.NewLine + "\t" + toy;
+            }
+
+            return message;
+
+        }
+
+    }
+}
