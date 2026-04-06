8c159a7bd56d94255c170e2882f44a54ecd7fc8a
Ackerman, Adam P. <adam@ou.edu>
2026-04-06 10:17:58 -0500

    Completed participation over just the Bunny class.  Need to add on the file input now

---
+++ Participations/Classes_Easterbunny/Bunny.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Easterbunny
+{
+    public class Bunny
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string DeliveryDate { get; set; }
+        public int BasketSize { get; set; }
+        public string EggColor { get; set; }
+        public int EggCount { get; set; }
+        public string Location { get; set; }
+        public string FavoriteColor { get; set; }
+
+        public Bunny()
+        {
+            Name = string.Empty;
+            Age = 0;
+            DeliveryDate = string.Empty;
+            BasketSize = 0;
+            EggColor = string.Empty;
+            EggCount = 0;
+            Location = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+
+        // Overloaded Constructor
+        public Bunny(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            DeliveryDate = string.Empty;
+            BasketSize = 0;
+            EggColor = string.Empty;
+            EggCount = 0;
+            Location = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+
+        public int HowManyEasterBaskets()
+        {
+            int baskets = 0;
+
+            baskets = (int)Math.Ceiling((double)EggCount / BasketSize);
+
+            return baskets;
+        }
+
+        public override string ToString()
+        {
+           return $"Bunny Name: {Name}, Age: {Age}, Delivery Date: {DeliveryDate}, Basket Size: {BasketSize}, Egg Color: {EggColor}, Egg Count: {EggCount}, Location: {Location}, Favorite Color: {FavoriteColor}";
+        }
+
+
+
+    }
+}
+
---
+++ Participations/Classes_Easterbunny/Program.cs
@@ -0,0 +1,26 @@
+﻿using Classes_Easterbunny;
+
+List<Bunny> bunnies = new List<Bunny>();
+
+Bunny bunny = new Bunny();
+bunny.Name = "Fluffy";
+bunny.Location = "South Oval";
+bunny.EggCount = 100;
+bunny.Age = 12;
+bunny.BasketSize = 12;
+bunny.DeliveryDate = "March 31, 2024";
+bunny.EggColor = "Pink";
+bunny.FavoriteColor = "Blue";
+
+bunnies.Add(bunny);
+
+
+
+
+
+
+foreach (Bunny b in bunnies)
+{
+    Console.WriteLine(b);
+    Console.WriteLine($"{b.Name} will need {b.HowManyEasterBaskets()} baskets to deliver all their eggs.");
+}
