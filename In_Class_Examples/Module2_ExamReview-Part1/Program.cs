47ea86f5bbc41389349cf4988f22de437017c227
Ackerman, Adam P. <adam@ou.edu>
2026-04-08 10:10:29 -0500

    COmpleted problem 1 of the review

---
+++ In_Class_Examples/Module2_ExamReview-Part1/Program.cs
@@ -0,0 +1,22 @@
+﻿do
+{
+    Console.WriteLine("Please enter the value, in feet <<");
+    double ft = Convert.ToDouble(Console.ReadLine());
+
+    double m = FeetToMeters(ft);
+
+    Console.WriteLine($"{ft.ToString("N")} converted to meters is {m.ToString("N")}");
+    Console.WriteLine("Do you have another value to convert? yes or no <<");
+} while (Console.ReadLine().ToLower() == "yes");
+
+
+
+// returnType FunctionName (parameters)
+double FeetToMeters(double feet)
+{
+    double meters = 0;
+
+    meters = feet * 0.3048;
+
+    return meters;
+}
