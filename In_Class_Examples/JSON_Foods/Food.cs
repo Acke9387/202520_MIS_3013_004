b2acd208d5c3e2a54efe28a7e189505ba73283d0
Ackerman, Adam P. <adam@ou.edu>
2026-04-22 10:43:48 -0500

    did an in class example over JSON

---
+++ In_Class_Examples/JSON_Foods/Food.cs
@@ -0,0 +1,38 @@
+﻿namespace JSON_Foods
+{
+    public class Food
+    {
+
+        public string name { get; set; }
+
+        public string category { get; set; }
+
+        public string region { get; set; }
+
+        public List<string> ingredients { get; set; }
+
+        public int calories { get; set; }
+
+        public double price_usd { get; set; }
+
+        public bool vegetarian { get; set; }
+
+
+        public Food()
+        {
+            name = string.Empty;
+            category = string.Empty;
+            region = string.Empty;
+            ingredients = new List<string>();
+            calories = 0;
+            price_usd = 0;
+            vegetarian = false;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}\nCategory: {category}\nRegion: {region}\nIngredients: {string.Join(", ", ingredients)}\nCalories: {calories}\nPrice (USD): {price_usd}\nVegetarian: {(vegetarian ? "Yes" : "No")}";
+        }
+
+    }
+}
---
+++ In_Class_Examples/JSON_Foods/Program.cs
@@ -0,0 +1,24 @@
+﻿using JSON_Foods;
+using Newtonsoft.Json;
+using System.Runtime.Intrinsics.Arm;
+
+string fileContents = File.ReadAllText("foods.json");
+
+FoodsAPI foodsAPI = JsonConvert.DeserializeObject<FoodsAPI>(fileContents);
+
+Console.WriteLine("All Foods:");
+//foreach (Food food in foodsAPI.american_foods)
+//{
+//    Console.WriteLine(food);
+//    Console.WriteLine();
+//}
+
+Console.WriteLine("All foods with a bun:");
+foreach (Food food in foodsAPI.american_foods)
+{
+    if (food.ingredients.Contains("bun"))
+    {
+        Console.WriteLine(food);
+        Console.WriteLine(); 
+    }
+}
