70f7ad6dbbeca4d2f68a39dfcf468b11d6f2ef64
Ackerman, Adam P. <adam@ou.edu>
2026-04-27 10:09:00 -0500

    Started participation

---
+++ Participations/JSON_From-A-File/Program.cs
@@ -0,0 +1,13 @@
+﻿using JSON_From_A_File;
+using Newtonsoft.Json;
+
+List<Owner> owners = new List<Owner>();
+string fileContents = File.ReadAllText("Car_Owners.json");
+
+owners  = JsonConvert.DeserializeObject<List<Owner>>(fileContents);
+
+foreach (var item in owners)
+{
+    Console.WriteLine(item);
+}
+
