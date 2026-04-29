6d2b338a6897396e8280b2e63b8d999483e434aa
Ackerman, Adam P. <adam@ou.edu>
2026-04-29 10:32:20 -0500

    completed participation over stock sales

--- Participations/JSON_Stock-Sales/Program.cs
+++ Participations/JSON_Stock-Sales/Program.cs
@@ -1,1 +1,41 @@
-﻿string fileContents = File.ReadAllText("stock_sales.json");
+﻿using JSON_Stock_Sales;
+using Newtonsoft.Json;
+
+string fileContents = File.ReadAllText("stock_sales.json");
+
+List<StockOption> stockOptions = JsonConvert.DeserializeObject<List<StockOption>>(fileContents);
+
+Console.WriteLine($"1.Display information for all stock transactions.\r\n2.Display only transactions for a specific stock symbol.\r\n3.Display only transactions where the investor made a profit.");
+int option = int.Parse(Console.ReadLine());
+
+switch (option)
+{
+    case 1: //all transactions
+        foreach (StockOption item in stockOptions)
+        {
+            Console.WriteLine(item);
+        }
+        break;
+    case 2: //specific stock symbol
+        Console.WriteLine("Enter the stock symbol:");
+        string symbol = Console.ReadLine();
+        foreach (StockOption item in stockOptions)
+        {
+            if (item.StockSymbol == symbol)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        break;
+    case 3: //Display only transactions where the investor made a profit
+        foreach (StockOption item in stockOptions)
+        {
+            if (item.IsProfit())
+			{
+				Console.WriteLine(item);
+            }
+        }
+        break;
+    default:
+        break;
+}
