200f4a77c7ba148e10d2eb7eb7da9d5608c3bf0b
Ackerman, Adam P. <adam@ou.edu>
2026-04-29 10:51:26 -0500

    completed in class example over API

---
+++ In_Class_Examples/JSON_API/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using JSON_API;
+using Newtonsoft.Json;
+
+var url = "https://rickandmortyapi.com/api/character";
+RickAndMortyAPI api = new RickAndMortyAPI();
+using (HttpClient client = new HttpClient())
+{
+    var response = await client.GetAsync(url);
+	if (response.IsSuccessStatusCode == true)
+	{
+		var json = await response.Content.ReadAsStringAsync(); 
+		api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+    }
+}
+
+foreach (var item in api.results)
+{
+    Console.WriteLine(item);
+}
