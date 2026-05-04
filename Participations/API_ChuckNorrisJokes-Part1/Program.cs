a6bd2ab51ca52269ffd06ce8880b40f0c5ba6cd0
Ackerman, Adam P. <adam@ou.edu>
2026-05-04 10:11:57 -0500

    Completed partcipiation over chuck norris api part 1

---
+++ Participations/API_ChuckNorrisJokes-Part1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using API_ChuckNorrisJokes_Part1;
+using Newtonsoft.Json;
+
+string url = "https://api.chucknorris.io/jokes/random";
+
+ChuckNorrisAPI joke = new ChuckNorrisAPI();
+
+do
+{
+    using (var client = new HttpClient())
+    {
+        var response = await client.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            joke = JsonConvert.DeserializeObject<ChuckNorrisAPI>(content);
+            Console.WriteLine(joke);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
+        }
+    }
+    Console.WriteLine("Do you want another joke? y/n");
+} while (Console.ReadLine().ToLower() == "y");
